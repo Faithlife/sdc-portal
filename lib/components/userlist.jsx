aa0ecb349c5fb45d56e0360c78dadbcae65ea7ed
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2015-04-14 13:04:40 -0700

    Shared: Add 'user' Actions & Store.

--- lib/components/userlist.jsx
+++ lib/components/userlist.jsx
@@ -1,33 +1,20 @@
 var littlest = require('littlest-isomorph');
 var React = require('react');
-var superagent = require('superagent');
 
 var UserList = React.createClass({
   mixins: [littlest.Mixin],
-  getInitialState: function () {
-    return ({
-      users: []
-    });
+  mappings: {
+    users: 'user:users'
   },
-
   componentDidMount: function () {
-    var self = this;
-
-    superagent
-      .get('/api/users')
-      .end(function (error, response) {
-        users = response.body.users;
-
-        if (self.isMounted()) {
-          self.setState({
-            users: users,
-          });
-        }
-      });
+    this.context.performAction('user:users');
   },
-
   render: function () {
     var self = this;
+
+    if (!self.state.users) {
+      return null;
+    }
 
     var users = self.state.users.map(function (user) {
       var href = '/machines?user=' + user;
